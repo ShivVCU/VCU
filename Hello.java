8decd8d83805eac879c01999edbf6e76467b3c3b
ShivVCU <69887752+ShivVCU@users.noreply.github.com>
2020-08-19 01:43:59 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,13 @@
+public class Hello {
+	
+	public static void main(String[] args) {
+
+	System.out.println("Shiv");
+	System.out.println("Lab 01");
+	System.out.println("Use Sublime Text on your computer to create this following program.");
+	System.out.println("corretto-8");
+	System.out.println("(2020FALL)INTRODUCTION TO PROGRAMMING - C90");
+	System.out.println();
+
+	} 
+}
