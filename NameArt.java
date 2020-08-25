d315b1ed52002b34a85d9e4bcb9afbf96077f3ba
ShivVCU <69887752+ShivVCU@users.noreply.github.com>
2020-08-25 17:17:06 -0400

    Add files via upload

--- NameArt.java
+++ NameArt.java
@@ -1,43 +1,72 @@
 /****************************************************************************
-* Java program name
+* NameArt
 **************************************************************************** 
-* Project description
+* This project outputs my name in a unique way on the display screen.
 *____________________________________________________________________________ 
-* Your name
-* The version date
-* The course number and section 
+* Shiv Sharma
+* 08 25, 2020
+* CMSC 255 Section C90
 ****************************************************************************/
 
 public class NameArt
 {
 	public static void main(String[] args)
 	{
-		System.out.println(" .----------------.   .----------------.  .----------------.  .------------------.");
-		System.out.println("| .--------------.  || .--------------. || .--------------. || .----------------. |");
-		System.out.println("| |    ________   | || |  ____  ____  | || |     _____    | || | ____     ____  | |");
-		System.out.println("| |   /  ___   |  | || | |_   ||   _| | || |    |_   _|   | || ||_  _|   |_  _| | |");
-		System.out.println("| |  |  (__ \\_|  | || |   | |__| |   | || |      | |     | || |  \\ \\   / /   | |");
-		System.out.println("| |   '.___`-.    | || |   |  __  |   | || |      | |     | || |   \\ \\ / /    | |");
-		System.out.println("| |  |`\\____) |  | || |  _| |  | |_  | || |     _| |_    | || |    \\  ' /     | |");
-		System.out.println("| |  |_______.'   | || | |____||____| | || |    |_____|   | || |     \\__/      | |");
-		System.out.println("| |               | || |              | || |              | || |                | |");
-		System.out.println("| '-------------- ' || '--------------' || '--------------' || '----------------' |");
-		System.out.println("'---------------- '  '----------------'  '----------------'  '------------------' ");
-		System.out.println();
-		
+System.out.println(" ");
+System.out.println("Shiv Sharma");
+System.out.println("Project 1 - Name in Code");
+System.out.println("This project outputs my name in a unique way on the display screen.");
+System.out.println("08 25, 2020");
+System.out.println("CMSC 225 Section C90");
+System.out.println(" ");
+
+
+System.out.println(" .----------------.  .----------------.  .----------------.  .------------------.");
+System.out.println("| .--------------. || .--------------. || .--------------. || .----------------. |");
+System.out.println("| |    _______   | || |  ____  ____  | || |     _____    | || |  ____   ____   | |");
+System.out.println("| |   /  ___  |  | || | |_   ||   _| | || |    |_   _|   | || | |_  _| |_  _|  | |");
+System.out.println("| |  |  (__ \\_|  | || |   | |__| |   | || |      | |     | || |   \\ \\   / /    | |");
+System.out.println("| |   '.___`-.   | || |   |  __  |   | || |      | |     | || |    \\ \\ / /     | |");
+System.out.println("| |  |`\\____) |  | || |  _| |  | |_  | || |     _| |_    | || |     \\ ' /      | |");
+System.out.println("| |  |_______.'  | || | |____||____| | || |    |_____|   | || |      \\_/       | |");
+System.out.println("| |              | || |              | || |              | || |                | |");
+System.out.println("| '--------------' || '--------------' || '--------------' || '----------------' |");
+System.out.println("'------------------''------------------''------------------''--------------------'  ");
+
+		System.out.println("	    _________________________________________________");
+		System.out.println("	   /                                                 \\");
+		System.out.println("	   |    _________________________________________    |");
+		System.out.println("	   |   |                                         |   |");
+		System.out.println("	   |   |  C:\\> _                                 |   |");
+		System.out.println("	   |   |                                         |   |");
+		System.out.println("	   |   |                  VCU                    |   |");
+		System.out.println("	   |   |           Computer Science              |   |");
+		System.out.println("	   |   |                #Ramily                  |   |");
+		System.out.println("	   |   |                                         |   |");
+		System.out.println("	   |   |              Shiv Sharma                |   |");
+		System.out.println("	   |   |                                         |   |");
+		System.out.println("	   |   |                                         |   |");
+		System.out.println("	   |   |                                         |   |");
+		System.out.println("	   |   |                                         |   |");
+		System.out.println("	   |   |                                         |   |");
+		System.out.println("	   |   |_________________________________________|   |");
+		System.out.println("	   |                                                 |");
+		System.out.println("	   \\_________________________________________________/");
+		System.out.println("		  \\___________________________________/");
+System.out.println("                ___________________________________________");
+System.out.println("             _-'    .-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-.  --- `-_");
+System.out.println("          _-'.-.-. .---.-.-.-.-.-.-.-.-.-.-.-.-.-.-.--.  .-.-.`-_");
+System.out.println("       _-'.-.-.-. .---.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-`__`. .-.-.-.`-_");
+System.out.println("    _-'.-.-.-.-. .-----.-.-.-.-.-.-.-.-.-.-.-.-.-.-.-----. .-.-.-.-.`-_");
+System.out.println(" _-'.-.-.-.-.-. .---.-. .-------------------------. .-.---. .---.-.-.-.`-_");
+System.out.println(":-------------------------------------------------------------------------:");
+System.out.println("`---._.-------------------------------------------------------------._.---'");
+System.out.println("");
+System.out.println("");
+
+
+System.out.println();
+
 
 	}
 }
-	/*	System.out.println(" .----------------.   .----------------.  .----------------.  .------------------.");
-		System.out.println("| .--------------.  || .--------------. || .--------------. || .----------------. |");
-		System.out.println("| |    ________   | || |  ____  ____  | || |     _____    | || | ____     ____  | |");
-		System.out.println("| |   /  ___   |  | || | |_   ||   _| | || |    |_   _|   | || ||_  _|   |_  _| | |");
-		System.out.println("| |  |  (__ \\_|  | || |   | |__| |   | || |      | |     | || |  \\ \\   / /   | |");
-		System.out.println("| |   '.___`-.    | || |   |  __  |   | || |      | |     | || |   \\ \\ / /    | |");
-		System.out.println("| |  |`\\____) |  | || |  _| |  | |_  | || |     _| |_    | || |    \\  ' /     | |");
-		System.out.println("| |  |_______.'   | || | |____||____| | || |    |_____|   | || |     \\__/      | |");
-		System.out.println("| |               | || |              | || |              | || |                | |");
-		System.out.println("| '-------------- ' || '--------------' || '--------------' || '----------------' |");
-		System.out.println("'---------------- '  '----------------'  '----------------'  '------------------' ");
-		System.out.println();
-		*/
