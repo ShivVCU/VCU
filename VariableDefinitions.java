c19addb3980b92ab13b47df176bcda62891b7c5f
ShivVCU <69887752+ShivVCU@users.noreply.github.com>
2020-09-02 20:20:10 -0400

    Add files via upload

---
+++ VariableDefinitions.java
@@ -0,0 +1,137 @@
+
+public class VariableDefinitions {
+	public static void main(String[]args) {
+		/*************************************
+* initial declaration of numPeople variable
+****************************************/
+byte numPeople = 3;
+
+		/*************************************
+* initial declaration of itemPrice variable
+****************************************/
+double itemPrice = 32.32;
+
+
+	/*************************************
+* initial declaration of roomNumber variable
+****************************************/
+int roomNumber = 207;
+
+/*************************************
+* initial declaration of charValue variable
+****************************************/
+char charValue = 'a';
+
+System.out.println("The numPeople variable has a value of " +
+numPeople);
+
+System.out.println("The itemPrice variable has a value of " +
+itemPrice);
+
+System.out.println("The roomNumber variable has a value of " +
+roomNumber);
+
+System.out.println("The charValue variable has a value of " +
+charValue);
+
+		/*************************************
+* modified value of numPeople variable
+****************************************/
+numPeople = 6;
+
+		/*************************************
+* modified value of itemPrice variable
+****************************************/
+itemPrice = 23.23;
+
+	/*************************************
+* modified value of roomNumber variable
+****************************************/
+roomNumber = 202;
+
+/*************************************
+* modified value of charValue variable
+****************************************/
+charValue = 'b';
+
+System.out.println("The numPeople variable now has a value of " +
+numPeople);
+
+System.out.println("The itemPrice variable now has a value of " +
+itemPrice);
+
+System.out.println("The roomNumber variable now has a value of " +
+roomNumber);
+
+System.out.println("The charValue variable now has a value of " +
+charValue);
+
+		/*************************************
+* initial declaration of numMeals constant
+****************************************/
+final byte numMeals = 4;
+
+		/*************************************
+* initial declaration of salesPrice constant
+****************************************/
+final double salesPrice = 30;
+
+
+	/*************************************
+* initial declaration of numberRank constant
+****************************************/
+final int numberRank = 21;
+
+/*************************************
+* initial declaration of firstInitial constant
+****************************************/
+final char firstInitial = 's';
+
+System.out.println("The numMeals constant has a value of " +
+numMeals);
+
+System.out.println("The salesPrice constant has a value of " +
+salesPrice);
+
+System.out.println("The numberRank constant has a value of " +
+numberRank);
+
+System.out.println("The firstInitial constant has a value of " +
+firstInitial);
+
+			/*************************************
+* modified numMeals constant
+****************************************/
+//numMeals = 10;
+
+		/*************************************
+* modified salesPrice constant
+****************************************/
+//salesPrice = 60;
+
+
+	/*************************************
+* modified numberRank constant
+****************************************/
+//numberRank = 15;
+
+/*************************************
+* modified firstInitial constant
+****************************************/
+//irstInitial = 'k';
+
+//System.out.println("The numMeals now constant has a value of " +
+//numMeals);
+
+//System.out.println("The salesPrice now constant has a value of " +
+//salesPrice);
+
+//System.out.println("The numberRank now constant has a value of " +
+//numberRank);
+
+//System.out.println("The firstInitial now constant has a value of " +
+//firstInitial);
+
+	}
+
+}
